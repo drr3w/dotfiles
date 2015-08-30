c2188340f2f9d2e2cb3e53a1b2797eab8f628bc0
etch <etch@Echo.local>
2015-08-29 21:02:23 -0700

    Fixed: - symlinking works properly now

--- shazaam.py
+++ shazaam.py
@@ -7,13 +7,9 @@ regex = r'.*rc\n'
 
 
 files = getoutput("ls")
-print "files: %s %s" % (files,type(files))
 
 rc_files = re.findall(regex, files, re.IGNORECASE)
 print rc_files
 for file in rc_files:
     file = file.strip('\n')
-    result = getoutput("ln -s %s ~/.%s") % (file, file)
-
-
-
+    result = getoutput("ln -s ~/.dotfiles/%s ~/.%s" % (file,file))

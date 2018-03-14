c3f380f7b92fcf2f9c50390f534a76bf819d47cd
drr3w <drew@moj.io>
2018-03-14 14:27:22 -0700

    fixed cd bug and added color

--- bin/pullall.sh
+++ bin/pullall.sh
@@ -6,10 +6,19 @@
 #  The only requirement is that the remotes setup in the repos
 #  use ssh and not https
 
-for dir in `find . -maxdepth 1 -type d`; do
-    cd $dir
-    if [ -d ".git" ]; then
-        git pull
+TC='\e['
+
+Bold="${TC}1m"    # Bold text only, keep colors
+Rst="${TC}0m"     # Reset all coloring and style
+Yellow="${TC}34m"; 
+
+for dir in `ls`; do
+    if [ -d $dir ]; then
+        cd $dir
+        if [ -d ".git" ]; then
+            echo -e "${Bold}${Yellow}$dir:${Rst}"
+            git pull
+        fi
         cd ..
     fi
 done

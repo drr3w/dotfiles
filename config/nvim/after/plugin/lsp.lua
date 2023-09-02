58bf4a4a7e704b11f49b6ed4c4b1efac2ff54626
Andrew Hersh <hersh4@llnl.gov>
2023-09-02 10:37:41 -0700

    Add null_ls config for python

--- config/nvim/after/plugin/lsp.lua
+++ config/nvim/after/plugin/lsp.lua
@@ -5,3 +5,22 @@ lsp.setup()
 
 local cmp = require('cmp')
 local cmp_mappings = lsp.defaults.cmp_mappings({[" "] = cmp.mapping.complete()})
+
+local null_ls = require('null-ls')
+
+null_ls.setup({
+  sources = {
+    null_ls.builtins.formatting.black,
+    null_ls.builtins.diagnostics.pylint,
+  }
+})
+
+local servers = { 'pyright' }
+for _, lsp in pairs(servers) do
+    require('lspconfig')[lsp].setup {
+        on_attach = on_attach,
+        flags = {
+          debounce_text_changes = 150,
+        }
+    }
+end

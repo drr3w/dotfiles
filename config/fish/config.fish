be0ea791bffe76fd499eeb1c857079433bcba028
Andrew Hersh <etch.himself@gmail.com>
2025-07-22 17:07:53 -0700

    updated fish. initial commit hypr configs.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,9 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-#function fish_prompt
-#    echo (set_color green)(hostname -s)(set_color normal) (prompt_pwd)' ' 
-#end
+set TERM "xterm"
 
 function fish_prompt
     # Host shortname
@@ -19,14 +17,30 @@ function fish_prompt
         set git_branch (git rev-parse --abbrev-ref HEAD 2>/dev/null)
 
         if test (git status --porcelain 2>/dev/null | wc -l) -gt 0
-            set git_dirty "❌" 
+            set git_dirty "✗" 
         else
-            set git_dirty "✅"
+            set git_dirty "✔"
         end
 
         set git_info (set_color green)"($git_branch $git_dirty)"
     end
 
     echo -n "$host: $cwd$git_info "
+end
+
+## Useful aliases
+
+alias vi 'nvim'
+alias ls 'eza -al --color=always --group-directories-first --icons' # preferred listing
+alias lsz 'eza -al --color=always --total-size --group-directories-first --icons' # include file size
+alias la 'eza -a --color=always --group-directories-first --icons'  # all files and dirs
+alias ll 'eza -l --color=always --group-directories-first --icons'  # long format
+alias lt 'eza -aT --color=always --group-directories-first --icons' # tree listing
+alias l. 'eza -ald --color=always --group-directories-first --icons .*' # show only dotfiles
+
+# Replace some more things with better alternatives
+alias cat 'bat --style header --style snip --style changes --style header'
+if not test -x /usr/bin/yay; and test -x /usr/bin/paru
+    alias yay 'paru'
 end
 
